8b677f85938160de20b904e91a32b2537b6f0064
Mariawillow <mani0013@stud.kea.dk>
2025-02-09 21:01:54 +0100

    rettelser i mappe

--- string opg/3caPitalization/3caPitalization.js
+++ string opg/3caPitalization/3caPitalization.js
@@ -1,4 +1,11 @@
+//Given a single name string in an unknown case, e.g. “peter” or “PETER” - create a new string with the same name, where the third letter is uppercase, and the rest is lowercase. I.e. “peTer”.
 
+// Hint: use substring, toUpperCase, toLowerCase and simple string concatenation
+
+
+// Test your code with various crazy combinations of your own name, like “pETer”, “PEter”, “peteR”, “PEtER” and so on.
+
+// Make sure that it also works with longer names. Don't worry about names shorter than 3 characters.
 
 const navn = {
     person: { text: "Peter", location: ".fullName" }
---
+++ string opg/Real Capitalization/realcapitalization.js
@@ -0,0 +1,15 @@
+// Capitalization
+// Use the 3caPitalization exercise as a basis for this exercise. Modify the code, so that only the first letter is upper case, and the rest is lower case.
+
+// Make sure that it can take names of any length!
+
+// Important: DO NOT look for solutions online - use the MDN documentation, and find the string methods that will help you. This exercise is about solving an unknown problem, not about finding existing solutions.
+
+// When complete, add, commit, and also paste the js-code here below.
+
+
+const navn = {
+    person: { text: "Peter", location: ".fullName" }
+  };
+  
+  console.log(navn.person.text); 
