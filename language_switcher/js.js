2527c70a12649b9073c66ef459af12e6b7fa1a4f
Mariawillow <mani0013@stud.kea.dk>
2025-02-10 10:28:22 +0100

    lang

--- language_switcher/js.js
+++ language_switcher/js.js
@@ -1,18 +1,42 @@
 "use strict";
+//Det er de samme inhold objekt, men i forskellige steder. 
 const texts = {
   de: {
+    //Nedenfor er der array tetxt
     texts: [
+      //Nedenfor er der de to objekter
       { text: "Das Bot", location: ".header" },
       { text: "Das Ro-Bot", location: ".footer" },
     ],
   },
   da: {
+        //Nedenfor er der array tetxt
     texts: [
+            //Nedenfor er der de to objekter
       { text: "Båden", location: ".header" },
       { text: "Robotten", location: ".footer" },
     ],
   },
 };
+
+// document.querySelector("#select").addEventListener("change", languageChange);
+// function languageChange(evt){
+//   // console.log(evt.target);
+//   locale = document.querySelector("#select").value;
+//   console.log("TEXTS", texts[locale].texts);
+//   texts[locale].texts.forEach(element => {
+//     document.querySelector(element.location).textContent = element.texts;
+//   });
+
+// }
+
+// let locale = "da";
+
+
+
+
+
+
 
 document.querySelector(".da").addEventListener("click", visDa);
 
