04618c40d23e7f025f1d453b16c5b2e5269c9a12
Mariawillow <mani0013@stud.kea.dk>
2025-02-10 21:30:28 +0100

    theme og profanity opg

--- theme_switcher/theme.js
+++ theme_switcher/theme.js
@@ -2,12 +2,16 @@ const theme = document.getElementById("colorSelect");
 const body = document.body;
 
 
-// Event listener der ændrer data-theme attributten
+// Event listener der ændrer data-filter attributten
+//Henter den nye værdi (value) fra det HTML-element, der udløste eventet (e.target).
+//e repræsenterer event-objektet, som indeholder oplysninger om ændringen.
 theme.addEventListener("change", (e) => {
+    //den nye farve (value/værdi) hentes. 
     const theme = e.target.value;
-    body.setAttribute("data-theme", theme);
+    //Sætter et data-filters-attribut på body-elementet.
+    body.setAttribute("data-filters", theme);
+    console.log(body);
   });
-console.log(theme);
 
 // const visLys =
 // document.querySelector(".lys").addEventListener("click", visLys);
