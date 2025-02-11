0e61b7a8c6ee07733616f5e3fa425840a1f89b6c
Mariawillow <mani0013@stud.kea.dk>
2025-02-11 20:15:57 +0100

    animals opg

--- animals/animals_objects.js
+++ animals/animals_objects.js
@@ -2,7 +2,51 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-const allAnimals = [];
+const Animal = {
+    name: "-defaulname",
+    desc: "-no description",
+    type: "-unknonw",
+    age: 0
+};
+
+const allAnimals =  [
+    //{
+//     name: "Mandu",
+//     desc: "amazing",
+//     type: "cat",
+//     age: 10
+// },
+// {
+//     name: "Mia",
+//     desc: "black", 
+//     type: "cat",
+//     age: 10
+// },
+// {
+//     name: "Leeloo",
+//     desc: "growing",
+//     type: "dog",
+//     age: 3
+// },
+// {
+//     name: "Toothless",
+//     desc: "trained", 
+//     type: "dragon",
+//     age: 14
+// },
+// {
+//     name: "ScoobyDoo",
+//     desc: "wondering",
+//     type: "dog",
+//     age: 58
+// },
+// {
+//     name: "Horsey",
+//     desc: "horsing",
+//     type: "horse",
+//     age: 10
+// }
+];
 
 function start( ) {
     console.log("ready");
@@ -22,8 +66,33 @@ function loadJSON() {
 
 function prepareObjects( jsonData ) {
     jsonData.forEach( jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
-        
+        //  Create new object 
+        const animal = Object.create(Animal);
+
+        //find or extract data from json object
+        const fullname = jsonObject.fullname;
+
+        //de forskellige ord bliver sat i variabbler 
+        const firstSpace = fullname.indexOf(" ");
+        const secondSpace = fullname.indexOf(" ", firstSpace +1);
+        const lastSpace = fullname.lastIndexOf(" ");
+
+        const name = fullname.substring(0, firstSpace);
+        const desc = fullname.substring(secondSpace +1, lastSpace);
+        const type = fullname.substring(lastSpace +1);
+
+        //Jeg skriver _ for at se om der er mellemrum noge steder hvor jeg ikke vil have dem.
+        //den viser variablerne for hver sætning 
+console.log(`name: _${name}_, desc: _${desc}_, type: _${type}_`);
+
+//put cleand data into newly created object
+animal.name = name;
+animal.desc = desc;
+animal.type = type;
+animal.age = jsonObject.age;
+
+allAnimals.push(animal);
+
         // TODO: MISSING CODE HERE !!!
     });
 
