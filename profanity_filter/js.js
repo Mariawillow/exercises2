a9d02ce08abb8d122eed7e854a9e4e2372980b96
Mariawillow <mani0013@stud.kea.dk>
2025-02-11 10:23:41 +0100

    profanity

--- profanity_filter/js.js
+++ profanity_filter/js.js
@@ -1,5 +1,6 @@
 const curseWords = [  {    bad: "var",    good: "const",  },  {    bad: "float",    good: "grid",  },  {    bad: "marquee",    good: "just don't",  },];
 let theText = document.querySelector("p").textContent;
+
 // console.log(curseWords);
 // // console.log(theText.replaceAll("bad").join("good"));
 
@@ -11,14 +12,34 @@ let theText = document.querySelector("p").textContent;
 
 // const JoinText = splitText.join(" ");
 // console.log(JoinText)
+const closeDialog = document.getElementById("closeDialog");
 
+
+//Vi tager fat i knappen, bå når der klikkes skal funktioen starte:
 document.querySelector("button").addEventListener("click", skiftOrd);
 
+//funcktionen starter:
 function skiftOrd() {
+    //Vi tager curseWords som er de ord der skal udskiftes.
     curseWords.forEach((word) => {
-        theText = theText.replaceAll(word.bad, word.good);
-    })};
-    console.log(theText);
-
-document.querySelector("p").textContent = theText;
+        //Vi laver en funktion som skal tage p 
+    let sentence = document.querySelector("p");
+    //innerHTML tager vores indhold inde i et element
+    sentence.innerHTML = sentence.innerHTML
+    //Vi splitter (fjerner) bad ord
+       .split(word.bad)
+       //Og siger vi skal bruge //Good
+        .join(`<span>${word.good}</span>`);
+    })
+    //
+document.querySelector("button").addEventListener("click", alleredeGode);
+function alleredeGode(){
+    document.querySelector("dialog").showModal();
+document;
+}
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+});
+};
+ 
 
