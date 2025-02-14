91b185cdaa034a376afa5933bd0826d907a3f413
Mariawillow <mani0013@stud.kea.dk>
2025-02-14 14:32:57 +0100

    viz

--- dta Viz/js.js
+++ dta Viz/js.js
@@ -1,10 +1,13 @@
-const id = "47057";
+
 const url = `https://kea-alt-del.dk/kata-distortion/`;
 
 function getData() {
     fetch(url)
     .then((res) => res.json())
     .then(visData);
+    // .then((queue) => Response.json(){
+        
+    // })
 }
 
 getData();
@@ -12,5 +15,8 @@ getData();
 setInterval (getData, 10000);
 
 function visData(data){
-    console.log(data);
+    document.querySelector(".showNumber")
+    document.querySelector(".showNumber").textContent = `Se tal: ${data.inQueue}`;
+    // console.log(data);
 }
+
